8075c0c419114269caac70ea24d7ce82d7e605cb
shunen.chen <shunen.chen@gmail.com>
2013-02-22 21:53:30 -0800

    Adding uuid into hashmap Signed-off-by: shunen.chen <shunen.chen@gmail.com>

--- test/hgm/db/setup_tables.clj
+++ test/hgm/db/setup_tables.clj
@@ -22,34 +22,65 @@
 ;)
 
 
-(with-client client
-   (create-table
-	playerTable
-    {:read_units 2 :write_units 2}
-	{:name "team" :type :string}
-	{:name "position_number" :type :string}
-  ))
-  
-(with-client client
-    (create-table
-	  teamTable
-      {:read_units 2 :write_units 2}
-	  {:name "team" :type :string}
-	  {:name "roster" :type :string}
-    ))
-	
-(with-client client
-  (create-table
-	pastGameTable
-    {:read_units 2 :write_units 2}
-	{:name "game_ID" :type :string}
-    ))
-	
-(with-client client
-  (create-table
-	liveGameTable
-    {:read_units 2 :write_units 2}
-	{:name "game_ID" :type :string}
-	{:name "game_clock_game_event_ID" :type :string}
-    ))
 
+Live_Game_Table
+ACTIVE
+game_ID
+game_clock_uuid
+10
+5
+
+(with-client client
+	 (create-table liveGameTable
+	{:read_units 10 :write_units 5}
+	{:name "game_ID" :type :string}
+	{:name "game_clock_uuid" :type :string})
+)
+
+(with-client client
+	 (create-table pastGameTable
+	{:read_units 10 :write_units 5}
+	{:name "game_ID" :type :string}	)
+)
+
+(with-client client
+	 (create-table playerTable
+	{:read_units 10 :write_units 5}
+	{:name "uuid" :type :string}
+	{:name "date-time-uuid
+" :type :string})
+)
+
+(with-client client
+	 (create-table teamTable
+	{:read_units 10 :write_units 5}
+	{:name "name" :type :string}
+	{:name "date-time-uuid" :type :string})
+)
+
+
+(with-client client
+	 (create-table userTable
+	{:read_units 10 :write_units 5}
+	{:name "identity" :type :string})
+)
+
+(with-client client
+	 (create-table playerGameStatsTable
+	{:read_units 10 :write_units 5}
+	{:name "player" :type :string}
+	{:name "game" :type :string})
+)
+
+(with-client client
+	(create-table gamePlayerStatsTable
+	{:read_units 10 :write_units 5}
+	{:name "game" :type :string}
+	{:name "player" :type :string})
+)
+(with-client client
+	(create-table teamGameStatsTable
+	{:read_units 10 :write_units 5}
+	{:name "team" :type :string}
+	{:name "game" :type :string})
+)
