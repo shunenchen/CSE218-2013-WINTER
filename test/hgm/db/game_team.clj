7c8ff87dea920711fe9498a2b0830b4ed18fb0ed
shunen.chen <shunen.chen@gmail.com>
2013-02-28 12:27:27 -0800

    Add delete/create test-team and test-game Signed-off-by: shunen.chen <shunen.chen@gmail.com>

--- test/hgm/db/game_team.clj
+++ test/hgm/db/game_team.clj
@@ -4,7 +4,7 @@
   (:use clojure.test)
   (:use hgm.db.core :reload)
   (:use hgm.db.clj_dynamo :reload))
-  
+
 (defn delete-all-team []
    (with-client client 
    (doseq [item (scan teamTable {})]
@@ -12,12 +12,23 @@
    (is (empty? (scan teamTable {}))))
 )
 
-(def playerIdMap {:BRAYDONCOBURN (uuid) :NICKLASGROSSMANN (uuid) :MIKEKNUBLE (uuid) :BRAYDENSCHENN (uuid) :HARRYZOLNIERCZYK (uuid) :SEANCOUTURIER (uuid) :WAYNESIMMONDS (uuid) :LUKESCHENN (uuid) :MATTREAD (uuid) :MAXIMETALBOT (uuid) :RUSLANFEDOTENKO (uuid) :BRUNOGERVAIS (uuid) :CLAUDEGIROUX (uuid) :ERIKGUSTAFSSON (uuid) :ZACRINALDO (uuid) :KIMMOTIMONEN (uuid) :DANNYBRIERE (uuid) :JAKUBVORACEK (uuid) :ILYABRYZGALOV (uuid) :BRIANBOUCHER (uuid) :MATTNISKANEN (uuid) :DERYKENGELLAND (uuid) :PAULMARTIN (uuid) :PASCALDUPUIS (uuid) :TANNERGLASS (uuid) :CHRISKUNITZ (uuid) :BRANDONSUTTER (uuid) :JAMESNEAL (uuid) :BEAUBENNETT (uuid) :MATTCOOKE (uuid) :CRAIGADAMS (uuid) :ROBERTBORTUZZO (uuid) :BROOKSORPIK (uuid) :JOEVITALE (uuid) :TYLERKENNEDY (uuid) :KRISLETANG (uuid) :EVGENIMALKIN (uuid) :SIDNEYCROSBY (uuid) :MARC-ANDREFLEURY (uuid) :TOMASVOKOUN (uuid) })
-(def teamIdMap   {:NHL_PIT (uuid) :NHL_PHI (uuid)})
+(defn delete-all-game []
+   (with-client client 
+   (doseq [item (scan gameTable {})]
+   (delete-item gameTable (create-key (:id item) )))
+   (is (empty? (scan gameTable {}))))
+)
+
+(defn get-uuid-map 
+	[]
+	(with-client client (get-item playerTable (create-key "0"))))
+	  
+(def pu-map (read-string ( (get-uuid-map) :playerHashList)))
+(def tu-map (read-string ( (get-uuid-map) :teamHashList)))
 
 (defn create-test-team 
-[]
-  (with-client client (put-item teamTable {:id (tu-map :NHL_PIT) :data "INFO" :info (str {:name "Flyers" :id (tu-map :NHL_PIT) })}))
+  []
+  (with-client client (put-item teamTable {:id (tu-map :NHL_PIT) :data "INFO" :info (str {:name "Penguins" :id (tu-map :NHL_PIT) })}))
   (with-client client (put-item teamTable {:id (tu-map :NHL_PIT) :data "GAMES" :games (str [])}))
   (with-client client (put-item teamTable {:id (tu-map :NHL_PHI) :data "INFO" :info (str {:name "Flyers" :id (tu-map :NHL_PHI) })}))
   (with-client client (put-item teamTable {:id (tu-map :NHL_PHI) :data "GAMES" :games (str [])}))
@@ -25,8 +36,4 @@
 
 (defn create-test-game 
   []
-  (with-client client (put-item teamTable {:id (tu-map :NHL_PIT) :data "INFO" :info (str {:name "Flyers" :id (tu-map :NHL_PIT) })}))
-  (with-client client (put-item teamTable {:id (tu-map :NHL_PIT) :data "GAMES" :games (str [])}))
-  (with-client client (put-item teamTable {:id (tu-map :NHL_PHI) :data "INFO" :info (str {:name "Flyers" :id (tu-map :NHL_PHI) })}))
-  (with-client client (put-item teamTable {:id (tu-map :NHL_PHI) :data "GAMES" :games (str [])}))
-)
+  (create-game {:startTime 1361388600 :awayTeam (tu-map :NHL_PIT) :homeTeam (tu-map :NHL_PHI)}))
