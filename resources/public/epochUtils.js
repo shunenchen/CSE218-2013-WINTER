b75c80cedc7ec80daf8cf5efca0f1a64cb7b1952
Eric Seidel <gridaphobe@gmail.com>
2013-03-13 17:25:26 -0700

    fix the MillisecToMinSec formatting

--- resources/public/epochUtils.js
+++ resources/public/epochUtils.js
@@ -1,18 +1,26 @@
 function EpochToDate(epoch) {
-	var date = new Date(epoch *1000);
+        var date = new Date(epoch *1000);
     return date.toLocaleDateString();
 }
 
 function MillisecToMinSec(sec) {
-	var x = sec;
-	var seconds = x % 60;
-	x /= 60
-	var minutes = x % 60;
-	
-	var ret = {};
-	ret["mins"] = Math.floor(minutes);
-	ret["secs"] = Math.floor(seconds);
-	
-	return ret;
+        var x = sec;
+        var seconds = x % 60;
+        x /= 60
+        var minutes = x;
+
+        var ret = {};
+        minutes = Math.floor(minutes).toString();
+        if (minutes.length === 1) {
+            minutes = "0" + minutes;
+        }
+        seconds = Math.floor(seconds).toString();
+        if (seconds.length === 1) {
+            seconds = "0" + seconds;
+        }
+        ret["mins"] = minutes;
+        ret["secs"] = seconds;
+
+        return ret;
 }
 
